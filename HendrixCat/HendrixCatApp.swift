02791562faf28a917c69d042291a362140e1bb00
Adam <adam.mabrouki@webwag.com>
2024-12-23 13:14:21 +0100

    first

---
+++ HendrixCat/HendrixCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HendrixCatApp.swift
+//  HendrixCat
+//
+//  Created by Adam Mabrouki on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HendrixCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
